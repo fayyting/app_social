ce7d448340e7a4f0fa0d905fbedeef64afb24b37
Fatma Yücel <fatmaakdas2019@gmail.com>
2022-12-30 01:16:56 +0300

    Sidebar linkleri eklendi.

--- src/components/navigation/Sidebar.js
+++ src/components/navigation/Sidebar.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Sidebar extends Component {
     render() {
@@ -7,10 +8,70 @@ class Sidebar extends Component {
                 <div className="position-sticky pt-3">
                     <ul className="nav flex-column">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home" aria-hidden="true"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
+                            <Link className="nav-link" aria-current="page" to="/">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home mb-2" aria-hidden="true"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
                                 Dashboard
-                            </a>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="position-sticky pt-3">
+                    <ul className="nav flex-column">
+                        <li className="nav-item">
+                            <Link className="nav-link" aria-current="page" to="/post">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home mb-2" aria-hidden="true"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
+                                Posts
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="position-sticky pt-3">
+                    <ul className="nav flex-column">
+                        <li className="nav-item">
+                            <Link className="nav-link" aria-current="page" to="/comment">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home mb-2" aria-hidden="true"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
+                                Comments
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="position-sticky pt-3">
+                    <ul className="nav flex-column">
+                        <li className="nav-item">
+                            <Link className="nav-link" aria-current="page" to="/album">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home mb-2" aria-hidden="true"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
+                                Albums
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="position-sticky pt-3">
+                    <ul className="nav flex-column">
+                        <li className="nav-item">
+                            <Link className="nav-link" aria-current="page" to="/post">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home mb-2" aria-hidden="true"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
+                                Photos
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="position-sticky pt-3">
+                    <ul className="nav flex-column">
+                        <li className="nav-item">
+                            <Link className="nav-link" aria-current="page" to="/todo">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home mb-2" aria-hidden="true"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
+                                Todos
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="position-sticky pt-3">
+                    <ul className="nav flex-column mb-2">
+                        <li className="nav-item">
+                            <Link className="nav-link" aria-current="page" to="/user">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home mb-2" aria-hidden="true"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
+                                Users
+                            </Link>
                         </li>
                     </ul>
                 </div>
